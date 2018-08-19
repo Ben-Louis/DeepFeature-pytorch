b399e737d265df2c47eb66a67219b96b1928d5eb
Ben-Louis <penglu2097@gmail.com>
2018-08-19 17:01:55 +0800

    update

---
+++ extractor.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+import torchvision
+from utils import Normalization, FeatureExtractor
+
+
+class DeepFeature(nn.Module):
+    def __init__(self, base_model='vgg19'):
+        super(DeepFeature, self).__init__()
+
+        # build model
+        vgg19_model = getattr(torchvision.models, base_model)(pretrained=True)
+        self.cnn_temp = vgg19_model.featuresg
+        self.model = FeatureExtractor()  # the new Feature extractor module network
+        conv_counter = 1
+        relu_counter = 1
+        batn_counter = 1
+
+        block_counter = 1
+        self.stage2layer = {}
+
+        for i, layer in enumerate(list(self.cnn_temp)):
+
+            if isinstance(layer, nn.Conv2d):
+                name = "conv_" + str(block_counter) + "_" + str(conv_counter) + "__" + str(i)
+                conv_counter += 1
+                self.model.add_layer(name, layer)
+
+            if isinstance(layer, nn.ReLU):
+                name = "relu_" + str(block_counter) + "_" + str(relu_counter) + "__" + str(i)
+                if relu_counter == 1:
+                    self.stage2layer[block_counter] = i
+                relu_counter += 1
+                self.model.add_layer(name, nn.ReLU(inplace=False))
+
+            if isinstance(layer, nn.MaxPool2d):
+                name = "pool_" + str(block_counter) + "__" + str(i)
+                batn_counter = relu_counter = conv_counter = 1
+                block_counter += 1
+                self.model.add_layer(name, nn.MaxPool2d((2, 2), ceil_mode=True))  # ***
+
+
+            if isinstance(layer, nn.BatchNorm2d):
+                name = "batn_" + str(block_counter) + "_" + str(batn_counter) + "__" + str(i)
+                batn_counter += 1
+                self.model.add_layer(name, layer)  # ***
+
+        self.model.eval()
+
+        # normalization
+        self.norm = Normalization()
+
+
+    def get_feat_with_layer(self, x, layers=None):
+        x = self.norm(x).contiguous()
+        if layers is None:
+            layers = list(range(len(self.model)))
+        return self.model(x, layers)
+
+    def forward(self, x, stages=[1,2,3,4,5]):
+        layers = [self.stage2layer[s] for s in stages]
+        features = self.get_feat_with_layer(x, layers)
+        return features
+
+
---
+++ utils.py
@@ -0,0 +1,40 @@
+import torch
+
+default_mean = [0.485, 0.456, 0.406]
+default_std = [0.229, 0.224, 0.225]
+
+class Normalization(torch.nn.Module):
+    def __init__(self, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+        super(Normalization, self).__init__()
+
+        mean = torch.FloatTensor(mean).view(-1, 1, 1)
+        std = torch.FloatTensor(std).view(-1, 1, 1)
+
+        self.register_buffer('mean', mean)
+        self.register_buffer('std', std)
+
+    def forward(self, x):
+        return (x - self.mean) / self.std
+
+    def recover(self, x):
+        return (x * self.std + self.mean).clamp(0, 1)
+
+
+class FeatureExtractor(nn.Sequential):
+    def __init__(self):
+        super(FeatureExtractor, self).__init__()
+
+    def add_layer(self, name, layer):
+        self.add_module(name, layer)
+
+    def forward(self, x, layers):
+        feats = []
+        end = max(layers)
+        for i, module in enumerate(self._modules):
+            x = self._modules[module](x)
+            if i in layers:
+                feats.append(x)
+            if i == end:
+                break
+        return feats
+        
